6772a3e00f8cb4f91f04ffeeb4a6e6a0827c6275
spoker28 <skarbaev2@gmail.com>
2023-06-14 10:50:56 +0300

    make gradlew executable (checking)

--- src/main/java/org/hse_app/Application.java
+++ src/main/java/org/hse_app/Application.java
@@ -2,6 +2,6 @@ package org.hse_app;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!1");
     }
 }
